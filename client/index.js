322a1d88e41da67908d6fb60e589f35c2009776f
daneielle-pont <danielleshereepont@outlook.com>
2022-06-09 22:40:44 +1200

    fix typo

--- client/index.js
+++ client/index.js
@@ -3,7 +3,7 @@ import { render } from 'react-dom'
 import { Provider } from 'react-redux'
 
 import App from './components/App'
-import Store from './store'
+import store from './store'
 
 document.addEventListener('DOMContentLoaded', () => {
   render(
