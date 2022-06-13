ef11ace21505dbe5c1203a8c61468ce8198c5c11
daneielle-pont <danielleshereepont@outlook.com>
2022-06-13 22:45:01 +1200

    set up active page reducer

--- client/reducers/activePage.js
+++ client/reducers/activePage.js
@@ -1,1 +1,7 @@
 // TODO: add active page property to the redux store through making a reducer.
+
+import { NAVIGATE } from '../actions/actionList'
+
+const initState = 'home'
+
+function activeReducer(state = initState, action) {}
